d5479d8930ced0b6b37e0541e7c980b77fb3fa1e
Sephin Sebastian E <sephinsebastian0830@gmail.com>
2024-07-02 15:33:30 +0530

    added guestbook functionality

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1619400001"
+    serviceWorkerVersion: "3995812915"
   }
 });
